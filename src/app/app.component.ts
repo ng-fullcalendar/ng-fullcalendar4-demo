6b74ed3220de4359eeaee912ae9a3003b747d1e2
Jamaks <jamakrus@gmail.com>
2019-03-15 16:38:40 +0300

    add fullcalendar beta4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-fullcalendar4-demo';
+export class AppComponent implements OnInit {
+  @ViewChild('calendar') el: ElementRef;
+  ngOnInit() {
+    const dateObj = new Date();
+    const yearMonth = dateObj.getUTCFullYear() + '-' + (dateObj.getUTCMonth() + 1);
+    const calendar = new Calendar(this.el.nativeElement, {
+      editable: true,
+      events: [{
+        title: 'Long Event',
+        start: yearMonth + '-07',
+        end: yearMonth + '-10'
+      }],
+      plugins: [ dayGridPlugin, interactionPlugin ]
+    });
+    calendar.render();
+  }
+
 }
